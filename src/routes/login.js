f87ecc3bd2ee7ef3768c2767d131a69ea65a61cd
Ahmed Galal <ahmedgalaldev@gmail.com>
2021-04-30 15:48:34 +0200

    add try and catch to login and register

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,26 +2,33 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const passport = require("passport");
 
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ where: { email } }).catch((err) => {
-    console.log("Error: ", err);
-  });
+    if (!email && passport) {
+      return res
+        .status(400)
+        .json({ message: "Please enter email and password!" });
+    }
+    const user = await User.findOne({ where: { email } }).catch((err) => {
+      console.log("Error: ", err);
+    });
 
-  if(!user){
-       return res
-         .status(400)
-         .json({ message: "Email or password does not match!" });
-  }
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Email or password does not match!" });
+    }
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(400).json({ message: "Unable to login" });
-  }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Unable to login" });
+    }
 
     const jwtToken = jwt.sign(
       { id: user.id, email: user.email },
@@ -29,12 +36,9 @@ router.post("/login", async (req, res) => {
     );
 
     res.json({ message: "Welcome Back!", token: jwtToken });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
-
-
-
-
-
-
--- src/routes/register.js
+++ src/routes/register.js
@@ -4,28 +4,31 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const userExist = await User.findOne({ where: { email } }).catch((err) => {
-    console.log("Error: ", err);
-  });
+    const userExist = await User.findOne({ where: { email } }).catch((err) => {
+      console.log("Error: ", err);
+    });
 
-  if (userExist) {
-    return res.status(409).json({ message: "User already exists" });
-  }
+    if (userExist) {
+      return res.status(409).json({ message: "User already exists" });
+    }
 
-  
-  const hashed = await bcrypt.hash(password, 8);
-  
-  const user = new User({ name, email,password:hashed });
+    const hashed = await bcrypt.hash(password, 8);
 
-  const savedUser = await user.save().catch((err) => {
-    console.log("Error: ", err);
-    res.status(500).json({ error: "Cannot register user" });
-  });
+    const user = new User({ name, email, password: hashed });
 
-  if (savedUser) {
-    return res.status(201).json({ message: "Thanks for registering" });
+    const savedUser = await user.save().catch((err) => {
+      console.log("Error: ", err);
+      res.status(500).json({ error: "Cannot register user" });
+    });
+
+    if (savedUser) {
+      return res.status(201).json({ message: "Thanks for registering" });
+    }
+  } catch (e) {
+    console.log(e);
   }
 });
 
